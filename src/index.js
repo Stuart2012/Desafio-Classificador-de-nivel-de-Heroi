e2110957e97aee366b23cf533bff9eef3b126b76
Stuart2012 <stuart2012@gmail.com>
2025-08-02 21:51:02 -0300

    Segundo Commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 let nome = "Stuart"
-let experiencia = 1000
+let experiencia = 5000
+let nivel = ""
 
+if (experiencia >= 0 && experiencia <= 1000) {
+    nivel = "Ferro" 
+}
+else if (experiencia >= 1001 && experiencia <= 2000){
+    nivel = "Bronze"
+}
+else if (experiencia >= 2001 && experiencia <= 5000){
+    nivel = "Prata"
+}
+else if (experiencia >= 5001 && experiencia <= 7000){
+    nivel = "Ouro"
+}
+else if (experiencia >= 7001 && experiencia <= 8000){
+    nivel = "Platina"
+}
+else if (experiencia >= 8001 && experiencia <= 9000){
+    nivel = "Ascendente"
+}
+else if (experiencia >= 9001 && experiencia <= 10000){
+    nivel = "Imortal"
+}
+else if (experiencia >= 10001){
+    nivel = "Radiante"
+}
+else {
+    nivel = "Desconhecido"
+}
+
+console.log ("O Herói de nome " + nome + " está no nível " + nivel)
